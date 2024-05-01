5a40727f08b21bc03b95cd5cc438c6591f4bd290
mariyamsaleem32 <164928689+mariyamsaleem32@users.noreply.github.com>
2024-05-01 15:52:07 +0500

    javascript

---
+++ javascript start/script.js
@@ -0,0 +1,13 @@
+// console.log("hello world");
+// console.log("mariyam saleem");
+// document.write("hello world");
+// alert("welcome to our website");
+// let first_name;
+// first_name ="mariyam";
+// let last_name;
+//  last_name =2024;
+// console.log(last_name);
+var birth = 43333;
+var name = "mariyam saleem";
+var email = "mariyamsaleeem87@gmail.com";
+alert(name + "" + email + "    " + birth);
